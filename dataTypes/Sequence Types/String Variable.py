1e419de1511cdff91939ad775f0e88032e0ade1d
ebrahimhossaincse <mdebrahimhossain.me@gmail.com>
2025-01-03 04:50:14 -0500

    add

---
+++ dataTypes/Sequence Types/String Variable.py
@@ -0,0 +1,10 @@
+# String examples
+string1 = "Hello, World!"
+string2 = 'Python is fun'
+string3 = """This is a 
+multi-line string"""
+
+print("Assign the value using double quote : ",string1)
+print("Assign the value using the single quote: ",string2)
+print("Assign the value using multi line: ",string3)
+
