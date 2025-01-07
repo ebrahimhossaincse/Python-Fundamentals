260d952c3606472088b96a7c8f609153c5309854
ebrahimhossaincse <mdebrahimhossain.me@gmail.com>
2025-01-07 08:14:21 -0500

    add

---
+++ dataTypes/Sequence Types/List Variable.py
@@ -0,0 +1,8 @@
+# List examples
+list1 = [1, 2, 3, 4]
+list2 = ['Python', 3.14, True]
+list3 = [[1, 2], [3, 4]]  # Nested list
+
+print(list1)
+print(list2)
+print(list3)
---
+++ dataTypes/Sequence Types/Tuple Variable.py
@@ -0,0 +1,8 @@
+# Tuple examples
+tuple1 = (1, 2, 3)
+tuple2 = ('Python', 3.14, True)
+tuple3 = (1,)  # Single-element tuple
+
+print(tuple1)
+print(tuple2)
+print(tuple3)
